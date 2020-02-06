fe0da3dd99d0b0b82966efb031b9663ddd6a2427
KYUNGHEE YANG <alicesister1@gmail.com>
2020-02-06 16:55:49 +0900

    패키지 이름 변경

--- src/test/java/com/directori/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/directori/book/springboot/web/HelloControllerTest.java
@@ -1,5 +1,4 @@
-package com.directori.book.springboot;
-
+package com.directori.book.springboot.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +6,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -25,18 +24,5 @@ public class HelloControllerTest {
     mvc.perform(get("/hello"))
         .andExpect(status().isOk())
         .andExpect(content().string(hello));
-  }
-
-  @Test
-  public void helloDtoTest() throws Exception {
-    String name = "hello";
-    int amount = 1000;
-    mvc.perform(
-        get("/hello/dto")
-            .param("name", name)
-            .param("amount", String.valueOf(amount)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.name", is(name)))
-        .andExpect(jsonPath("$.amount", is(amount)));
   }
 }
