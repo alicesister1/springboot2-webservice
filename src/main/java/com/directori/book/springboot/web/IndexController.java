41a197b55de2332712c0be480e4d7ff1e200571c
KYUNGHEE YANG <alicesister1@gmail.com>
2020-02-10 10:59:06 +0900

    게시글 조회/수정/삭제 화면 추가

--- src/main/java/com/directori/book/springboot/web/IndexController.java
+++ src/main/java/com/directori/book/springboot/web/IndexController.java
@@ -1,21 +1,42 @@
 package com.directori.book.springboot.web;
 
+import com.directori.book.springboot.service.posts.PostsService;
+import com.directori.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-  @GetMapping("/")
-  public String index() {
+  private final PostsService postsService;
 
-    // mustache start는 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
-    // 기본 경로는 src/main/resources/templates
-    return "index"; // same as src/main/resources/templates/index.mustache
+//  @GetMapping("/")
+//  public String index() {
+//
+//    // mustache start는 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
+//    // 기본 경로는 src/main/resources/templates
+//    return "index"; // same as src/main/resources/templates/index.mustache
+//  }
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("posts", postsService.findAllDesc());
+    return "index";
   }
 
   @GetMapping("/posts/save")
   public String postsSave() {
     return "posts-save";
+  }
+
+  @GetMapping("/posts/update/{id}")
+  public String postsUpdate(@PathVariable Long id, Model model) {
+    PostsResponseDto dto = postsService.findById(id);
+    model.addAttribute("post", dto);
+
+    return "posts-update";
   }
 }
---
+++ src/main/java/com/directori/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,20 @@
+package com.directori.book.springboot.web.dto;
+
+import com.directori.book.springboot.domain.posts.Posts;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class PostsListResponseDto {
+  private Long id;
+  private String title;
+  private String author;
+  private LocalDateTime modifiedDate;
+
+  public PostsListResponseDto(Posts entity) {
+    this.id = entity.getId();
+    this.title = entity.getTitle();
+    this.author = entity.getAuthor();
+    this.modifiedDate = entity.getModifiedDate();
+  }
+}
