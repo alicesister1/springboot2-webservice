98887107c0eecdad5f284efa537e4c34e35ca962
KYUNGHEE YANG <ykh@soulint.com>
2020-03-04 10:37:17 +0900

    devserver proxy 설정 및 심볼릭 링크 연결 > cd 프로젝트루트\src\main > mklink /D client resources\static

--- src/main/java/com/directori/book/springboot/web/IndexController.java
+++ src/main/java/com/directori/book/springboot/web/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,13 +19,19 @@ public class IndexController {
   private final PostsService postsService;
   private final HttpSession httpSession;
 
-//  @GetMapping("/")
-//  public String index() {
-//
-//    // mustache starter는 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
-//    // 기본 경로는 src/main/resources/templates
-//    return "index"; // same as "src/main/resources/templates/index.mustache"
-//  }
+  /**
+   *  mustache starter는 컨트롤러에서 문자열을 반환할 때,
+   *  경로와 파일 확장자는 자동으로 지정됨
+   *  기본 경로는 src/main/resources/templates
+   */
+  /*
+  @RequestMapping("/")
+  public String home(Model model) {
+    model.addAttribute("message", "Hello, World!");
+    return "index";
+  }
+  */
+
   @GetMapping("/")
   public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/main/resources/static/js/app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname),
-        exclude: /(node_module)|(dist)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
+module.exports = (env) => {
+
+  /**
+  * 스프링부트 View 파일이 최종적으로 정적 리소스 경로에(src/main/resources/static)
+  * 있어야 하지만 클라이언트 코드를 수정할 때마다 static 디렉토리에 아웃풋 파일이 반영되면
+  * 핫스왑 기능이 활성화 되어 불필요한 컨텍스트 리로드가 발생할 수 있기 때문에
+  * client 경로를 별도로 지정함
+  */
+
+  // symbolic link 연결 후 정적 파일 라이브로 반영됨
+  let clientPath = path.resolve(__dirname, './src/main/client/js/app');
+  let outputPath = path.resolve(__dirname, 'dist');
+
+  return {
+    mode: !env ? 'development' : env,
+    entry: {
+      index: clientPath + '/index.js'
+    },
+    output: {
+      filename: '[name].js',
+      path: outputPath,
+    },
+    devServer: {
+      contentBase: outputPath,
+      publicPath: '/',
+//      host: '0.0.0.0',
+      host: 'localhost',
+      port: 8081,
+      proxy: {
+        '**': 'http://[::1]:8080',
       },
-      {
-        test: /\.mustache$/,
-        use: {
-          loader: 'mustache-loader'
-        }
-      }
-    ]
+      inline: true,
+      hot: false
+    }
   }
 };
