2b36eaed62917b90533661624ef2e2389fea3295
KYUNGHEE YANG <alicesister1@gmail.com>
2022-02-05 23:29:37 +0900

    글 등록 실패 시 오류 수정

--- src/main/java/com/directori/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/directori/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -25,20 +25,23 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-    OAuth2UserService delegate = new DefaultOAuth2UserService();
+    OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
     OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-    // 현재 로그인 진행 중인 서비스를 구분하는 코드, 추후 네이버, 카카오, 구글 로그인 구분하기 위해 사용됨
+    /*
+     * 현재 로그인 한 서비스를 구분하는 코드.
+     * 추후 네이버, 카카오, 구글 로그인 구분하기 위해 사용됨.
+     */
     String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-    /**
+    /*
      * OAuth2 로그인 진행시 키 값 (= PK). 구글의 경우 기본 코드 "sub",
      * 네이버 카카오 등은 기본 지원 않 함. 추후 네이버 로그인과 구글 로그인을 동시 지원할 때 사용됨
      */
     String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
         .getUserInfoEndpoint().getUserNameAttributeName();
 
-    /**
+    /*
      * OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스
      */
     OAuthAttributes attributes = OAuthAttributes
